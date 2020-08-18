e3f0f58f6b21fd972b942b77407b0f7017ee2600
tukuanchung <tukuanchung@gmail.com>
2020-08-18 16:27:19 +0800

    使用 Model-driven Form 驗證表單資料

--- src/app/posts/editor/editor.component.ts
+++ src/app/posts/editor/editor.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { FormGroup, FormControl, FormArray, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-editor',
@@ -7,21 +7,25 @@ import { FormGroup, FormControl, FormArray } from '@angular/forms';
   styleUrls: ['./editor.component.css']
 })
 export class EditorComponent implements OnInit {
+  constructor() { }
+  get tags(): FormArray {
+    return this.post.get('tags') as FormArray;
+  }
   post = new FormGroup({
-    title: new FormControl('default title'),
-    body: new FormControl(),
+    title: new FormControl('default title' , Validators.required),
+    body: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(10)
+    ]),
     tags: new FormArray([
       new FormControl('Angular'),
       new FormControl('HTML'),
       new FormControl('CSS')
     ])
   });
-  constructor() { }
 
+  git;
   ngOnInit() {
-  }
-  get tags(): FormArray {
-    return this.post.get('tags') as FormArray;
   }
 
   addTag(tag){
@@ -36,5 +40,12 @@ export class EditorComponent implements OnInit {
 
   createPost(){
     console.log(this.post.value);
+  }
+  get title(): AbstractControl{
+    return this.post.get('title');
+  }
+
+  get body(): AbstractControl{
+    return this.post.get('body');
   }
 }
