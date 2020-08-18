e4e58fe093446442e642f0bcdd61df787bf77d20
tukuanchung <tukuanchung@gmail.com>
2020-08-18 18:47:02 +0800

    使用 JWT 進行登入與身分認證

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
-import { PostsModule } from './posts/posts.module';
+import { ApiInterceptor } from './api.interceptor';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, HttpClientModule, LayoutModule, AppRoutingModule],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: ApiInterceptor,
+    multi: true,
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/posts/editor/editor.component.ts
+++ src/app/posts/editor/editor.component.ts
@@ -1,5 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormArray, Validators, AbstractControl } from '@angular/forms';
+import { FormGroup, FormControl, FormArray, Validators, AbstractControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-editor',
@@ -7,26 +8,43 @@ import { FormGroup, FormControl, FormArray, Validators, AbstractControl } from '
   styleUrls: ['./editor.component.css']
 })
 export class EditorComponent implements OnInit {
-  constructor() { }
+  post: FormGroup;
+
+  constructor(private fb: FormBuilder, private http: HttpClient) { }
+
+  ngOnInit() {
+    this.post = this.fb.group({
+      title: this.fb.control('', {
+        validators: [ Validators.required]
+      }),
+      body: this.fb.control('', {
+        validators: [Validators.required, Validators.minLength(10)]
+      }),
+      tags: this.fb.array([
+        this.fb.control('Angular'),
+        this.fb.control('HTML'),
+        this.fb.control('CSS'),
+      ])
+    });
+  }
+
   get tags(): FormArray {
     return this.post.get('tags') as FormArray;
   }
-  post = new FormGroup({
-    title: new FormControl('default title' , Validators.required),
-    body: new FormControl(null, [
-      Validators.required,
-      Validators.minLength(10)
-    ]),
-    tags: new FormArray([
-      new FormControl('Angular'),
-      new FormControl('HTML'),
-      new FormControl('CSS')
-    ])
-  });
+  // post = new FormGroup({
+  //   title: new FormControl('default title' , Validators.required),
+  //   body: new FormControl(null, [
+  //     Validators.required,
+  //     Validators.minLength(10)
+  //   ]),
+  //   tags: new FormArray([
+  //     new FormControl('Angular'),
+  //     new FormControl('HTML'),
+  //     new FormControl('CSS')
+  //   ])
+  // });
 
-  git;
-  ngOnInit() {
-  }
+
 
   addTag(tag){
     if (tag){
@@ -40,12 +58,19 @@ export class EditorComponent implements OnInit {
 
   createPost(){
     console.log(this.post.value);
+    this.http.post('http://localhost:3000/api/articles', {
+      article: this.post.value
+    }).subscribe(() => {
+      alert('susscess create article!');
+    }, (error) => {
+      alert(JSON.stringify(error));
+    });
   }
   get title(): AbstractControl{
     return this.post.get('title');
   }
 
   get body(): AbstractControl{
-    return this.post.get('body');
+    return this.post.get('title');
   }
 }
